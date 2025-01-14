7d6f6ad321caeecbe50d587535c78ac9c9e7d814
AhmadYousif89 <ayob.4wrk89@gmail.com>
2025-01-14 23:09:46 +0200

    Update dashboard content and add click counter

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,18 +1,16 @@
 import $ from 'jquery';
 import _ from 'lodash';
 
-const updateCounter = () => {
-  let times = 0;
-  $('button').on('click', () => $('#count').html(`${++times} clicks on the button`));
-};
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
 
-$(function () {
-  $('body').append('<p>ALX Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<button>Click here to get started</button>');
-  $('body').append('<p id="count"></p>');
-  $('body').append('<p>Copyright - ALX</p>');
+let count = 0;
 
-  updateCounter();
-  _.debounce(updateCounter, 500);
-});
+function updateCounter() {
+  $('#count').html(`${++count} clicks on the button`);
+}
+
+$('button').on('click', _.debounce(updateCounter, 500));
