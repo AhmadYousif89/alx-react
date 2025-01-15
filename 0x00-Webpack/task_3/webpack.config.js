29bf85a1b3cacab34cb2a875114be1a72b9fcdd2
AhmadYousif89 <ayob.4wrk89@gmail.com>
2025-01-15 22:39:30 +0200

    Task 03 and fix minor bugs

---
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    header: './header/header.js',
+    body: './body/body.js',
+    footer: './footer/footer.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'public'),
+    sourceMapFilename: '[file].map',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 8564,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: ['file-loader', { loader: 'image-webpack-loader' }],
+      },
+    ],
+  },
+  performance: {
+    maxAssetSize: 1024 * 1024, // 1 MB,
+    maxEntrypointSize: 1024 * 1024 * 2, // 2 MB
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  plugins: [new CleanWebpackPlugin(), new HtmlWebpackPlugin()],
+};
